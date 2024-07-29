88c4e58d67a7903e5ec01ab8c70d847fd1e045a2
Tsotne Darjania <98889556+TsotneDarjania@users.noreply.github.com>
2024-07-29 04:34:59 +0400

    FEATURE: Add view engine EJS and pageRouter

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
 import { PORT } from "./config";
+import pageRouter from "./routes/pageRouter";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Config
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+app.use(cors());
+
+app.use(express.static("static"));
+
+app.get("/", pageRouter);
 
 export function startServer() {
   app.listen(PORT, () => {
